58e357bbadbe0c5734dcaab668015858c6318524
Adam Strauch <cx@initd.cz>
2012-11-02 08:34:22 +0100

    Rename model field

--- ordered_model/models.py
+++ ordered_model/models.py
@@ -5,36 +5,36 @@ from django.db import models
 class OrderedModel(models.Model):
     """
     An abstract model that allows objects to be ordered relative to each other.
-    Provides an ``order`` field.
+    Provides an ``order_index`` field.
     """
     
-    order = models.PositiveIntegerField(editable=False)
+    order_index = models.PositiveIntegerField(editable=False)
     
     class Meta:
         abstract = True
-        ordering = ('order',)
+        ordering = ('order_index',)
     
     def save(self, *args, **kwargs):
         if not self.id:
-            qs = self.__class__.objects.order_by('-order')
+            qs = self.__class__.objects.order_by('-order_index')
             try:
-                self.order = qs[0].order + 1
+                self.order_index = qs[0].order + 1
             except IndexError:
-                self.order = 0
+                self.order_index = 0
         super(OrderedModel, self).save(*args, **kwargs)
     
     def _move(self, up):
         qs = self.__class__._default_manager
         if up:
-            qs = qs.order_by('-order').filter(order__lt=self.order)
+            qs = qs.order_by('-order_index').filter(order__lt=self.order_index)
         else:
-            qs = qs.filter(order__gt=self.order)
+            qs = qs.filter(order__gt=self.order_index)
         try:
             replacement = qs[0]
         except IndexError:
             # already first/last
             return
-        self.order, replacement.order = replacement.order, self.order
+        self.order_index, replacement.order = replacement.order, self.order_index
         self.save()
         replacement.save()
     
